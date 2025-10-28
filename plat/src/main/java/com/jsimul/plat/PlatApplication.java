e71de6e208000d4c234c7567082c05201c8c5178
Waiting-Chai <waitingchai45@gmail.com>
2025-10-28 18:27:19 +0800

    feat(init): init project;

---
+++ plat/src/main/java/com/jsimul/plat/PlatApplication.java
@@ -0,0 +1,11 @@
+package com.jsimul.plat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatApplication.class, args);
+    }
+}
